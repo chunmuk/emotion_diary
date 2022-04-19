54cb3feb241178db5dbb8d452824876239b1e0a0
Chunmuk Won <dnjscnsanr7@gamil.com>
2022-04-19 19:33:22 +0900

    first commit

---
+++ src/components/ControlMenu.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const ControlMenu = ({ value, onChange, optionList }) => {
+  return (
+    <select
+      className="ControlMenu"
+      value={value}
+      onChange={(e) => onChange(e.target.value)}
+    >
+      {optionList.map((it, idx) => (
+        <option key={idx} value={it.value}>
+          {it.name}
+        </option>
+      ))}
+    </select>
+  );
+};
+
+export default React.memo(ControlMenu);
---
+++ src/components/DiaryList.js
@@ -0,0 +1,80 @@
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import DiaryItem from "./DiaryItem";
+import MyButton from "./MyButton";
+import ControlMenu from "./ControlMenu";
+
+const sortOptionList = [
+  { value: "latest", name: "최신순" },
+  { value: "oldest", name: "오래된 순" },
+];
+
+const filterList = [
+  { value: "all", name: "전부다" },
+  { value: "good", name: "좋은 감정만" },
+  { value: "bad", name: "안좋은 감정만" },
+];
+
+const DiaryList = ({ diaryList }) => {
+  const navigate = useNavigate();
+  const [sortType, setSortType] = useState("latest");
+  const [filter, setFilter] = useState("all");
+
+  const getProcessedDiaryList = () => {
+    const filterCallBack = (item) => {
+      if (filter === "good") {
+        return parseInt(item.emotion) <= 3;
+      } else {
+        return parseInt(item.emotion) > 3;
+      }
+    };
+    const compare = (a, b) => {
+      if (sortType === "latest") {
+        return parseInt(b.date) - parseInt(a.date);
+      } else {
+        return parseInt(a.date) - parseInt(b.date);
+      }
+    };
+    const copyList = JSON.parse(JSON.stringify(diaryList));
+    const filteredList =
+      filter === "all" ? copyList : copyList.filter((it) => filterCallBack(it));
+    const sortedList = filteredList.sort(compare);
+    return sortedList;
+  };
+  return (
+    <div className="DiaryList">
+      <div className="menu_wrapper">
+        <div className="left_col">
+          <ControlMenu
+            value={sortType}
+            onChange={setSortType}
+            optionList={sortOptionList}
+          />
+          <ControlMenu
+            value={filter}
+            onChange={setFilter}
+            optionList={filterList}
+          />
+        </div>
+        <div className="right_col">
+          <MyButton
+            text={"새 일기 쓰기"}
+            type={"positive"}
+            onClick={() => {
+              navigate("/new");
+            }}
+          />
+        </div>
+      </div>
+      {getProcessedDiaryList().map((it) => (
+        <DiaryItem key={it.id} {...it} />
+      ))}
+    </div>
+  );
+};
+
+DiaryList.defaultProps = {
+  diaryList: [],
+};
+
+export default DiaryList;
